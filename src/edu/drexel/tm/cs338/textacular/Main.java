aadf4c36f35e35c1c64b214b0e04f46e5cb472b9
axocomm <axocomm@gmail.com>
2014-03-11 18:15:35 -0400

    Begin ProjectFrame

--- src/edu/drexel/tm/cs338/textacular/Main.java
+++ src/edu/drexel/tm/cs338/textacular/Main.java
@@ -1,7 +1,15 @@
 package edu.drexel.tm.cs338.textacular;
 
+import java.awt.EventQueue;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+    public static void main(String[] args) {
+        final ProjectFrame frame = new ProjectFrame();
+        
+        EventQueue.invokeLater(new Runnable() {
+        	public void run() {
+        		frame.setVisible(true);
+        	}
+        });
+    }
 }
---
+++ src/edu/drexel/tm/cs338/textacular/ProjectFrame.java
@@ -0,0 +1,12 @@
+package edu.drexel.tm.cs338.textacular;
+
+import javax.swing.JFrame;
+
+public class ProjectFrame extends JFrame {
+	public ProjectFrame() {
+		super("TeXtacular");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 600);
+	}
+}
