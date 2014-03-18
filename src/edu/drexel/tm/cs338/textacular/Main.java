5613097db2972400f2d58632c5836b2064b1f6b1
axocomm <axocomm@gmail.com>
2014-03-18 01:15:02 -0400

    Fix odd indentation and add comments to Main

--- src/edu/drexel/tm/cs338/textacular/Main.java
+++ src/edu/drexel/tm/cs338/textacular/Main.java
@@ -6,27 +6,40 @@ import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+/**
+ * The class Main.
+ *
+ * The main driver of the application
+ *
+ * @author Trevor Maglione <tm@cs.drexel.edu>
+ */
 public class Main {
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
     public static void main(String[] args) {
         final ProjectFrame frame = new ProjectFrame();
-        
+
         try {
-			UIManager.setLookAndFeel("com.pagosoft.plaf.PgsLookAndFeel");
-			SwingUtilities.updateComponentTreeUI(frame);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
-        
+            UIManager.setLookAndFeel("com.pagosoft.plaf.PgsLookAndFeel");
+            SwingUtilities.updateComponentTreeUI(frame);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
         EventQueue.invokeLater(new Runnable() {
-        	public void run() {
-        		frame.setVisible(true);
-        	}
+            public void run() {
+                frame.setVisible(true);
+            }
         });
     }
 }
